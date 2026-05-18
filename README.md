4fdb010752638ee4a8e83071a8b2a1f354875f75
MICHEL Christophe <christophe.michel@externe.maif.fr>
2026-05-18 14:40:55 +0200

    update with 2 routes

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # FLASK
 
-j'ai voulu tester rapidement. j'ai donc fait trois pages web.
+j'ai voulu tester rapidement. j'ai donc fait 5 pages web.
 - Une page d'accueil avec un lien vers une autre page
 - Une page avec un formulaire qui demande un prénom et un nom
 - une page qui affiche le prénom et le nom
+- Une page qui affiche qu'un texte
+- Une page qui affiche un json
 
 
 Rien d'extraordinaire mais c'est juste pour tester. Il n'y a pas vraiment de css.
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # importe le module Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 # créatio de l'instance du server web
 app = Flask(__name__)
@@ -25,3 +25,11 @@ def dashboard():
     print(lastname)
 
     return render_template("dashboard.html", firstname=firstname, lastname=lastname)
+
+@app.route("/text")
+def text():
+    return "page qui retourne uniquement un texte"
+
+@app.route("/json")
+def json():
+    return jsonify({"texte": "un objet json", "key": "value"})
